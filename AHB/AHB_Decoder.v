8936274be95c8762e5f4178323f6d588602697fe
Abhijeet J <jabhijeet301@gmail.com>
2025-03-25 22:25:09 +0530

    Refactor(Decoder):Refactored AHB_Decoder for Improved Scalability and Efficiency

--- AHB/AHB_Decoder.v
+++ AHB/AHB_Decoder.v
@@ -1,28 +1,19 @@
 module AHB_Decoder #(
     parameter DATA_WIDTH = 32,   
     parameter ADDRESS_WIDTH = 32, 
-    parameter NO_OF_SLAVES = 2
+    parameter NO_OF_SLAVES = 2,
+    parameter SLAVE_ADDR_BITS = 23
 )(
     input  [ADDRESS_WIDTH-1:0] HADDR,  
-    output reg [NO_OF_SLAVES-1:0] HSEL 
+    output reg [NO_OF_SLAVES-1:0] HSEL
 );
 
-    localparam SLAVE_ADDR_BITS = $clog2(NO_OF_SLAVES); 
-
-    always @(*) begin
-        HSEL = {NO_OF_SLAVES{1'b0}}; 
-        
-        case (HADDR[ADDRESS_WIDTH-1:ADDRESS_WIDTH-SLAVE_ADDR_BITS]) 
-            0: HSEL = (1 << 0);
-            1: HSEL = (1 << 1);
-            2: HSEL = (1 << 2);
-            3: HSEL = (1 << 3);
-            4: HSEL = (1 << 4);
-            5: HSEL = (1 << 5);
-            6: HSEL = (1 << 6);
-            7: HSEL = (1 << 7);
-            8: HSEL = (1 << 8);
-            default: HSEL = {NO_OF_SLAVES{1'b0}}; 
-        endcase
+always @(*) begin
+    HSEL = {NO_OF_SLAVES{1'b0}};
+    
+    if (HADDR[ADDRESS_WIDTH-1:ADDRESS_WIDTH-SLAVE_ADDR_BITS] < NO_OF_SLAVES) begin
+        HSEL[HADDR[ADDRESS_WIDTH-1:ADDRESS_WIDTH-SLAVE_ADDR_BITS]] = 1'b1;
     end
+end
+
 endmodule
